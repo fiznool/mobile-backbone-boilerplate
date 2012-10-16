64d1a2c5091af27ead570424e0191f070bf9d814
fiznool <fiznool@gmail.com>
2012-10-16 15:42:35 +0100

    Added app events registration

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,12 +10,38 @@ define(function(require) {
     root: "/"
   };
 
+  // Convenience function for registering a method as an event
+  var _registerWith = function(app, namespace, context) {
+    return function(item, key) {
+      if (_.isFunction(item)) {
+        // Add an event listener on this function which
+        // can be accessed via app.trigger() at any time
+        app.on(namespace + ':' + key, function() {
+          item.apply(context, arguments);
+        });
+      }
+    };
+        
+  };
+
   // Mix Backbone.Events into the app object.
   return _.extend(app, {
-    // Create a custom object with a nested Views object.
-    module: function(additionalProps) {
-      return _.extend({ Views: {} }, additionalProps);
+    
+    // A way to register modules for application-wide events.
+    registerModule: function(namespace, module, context) {
+      var that = this;
+
+      if (!context) {
+        context = module;
+      }
+
+      // Register each function in the module
+      // as an event that can be called
+      // via app.trigger() at a later date
+      _.each(module, _registerWith(this, namespace, context));
+
     }
+
   }, Backbone.Events);
 
 });
