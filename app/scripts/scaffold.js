cd7b4de6824563807e83cf8d1df8744d9d83b0fc
James Ward <jamesward1101@gmail.com>
2012-11-15 15:11:12 +0000

    fix updateRegion given a template, views object

--- app/scripts/scaffold.js
+++ app/scripts/scaffold.js
@@ -9,103 +9,9 @@ define(function(require) {
   require('plugins/backbone.super');
   require('plugins/backbone.layoutmanager');
   
-  // Activity constructor
-  var Activity = function(options) {
-    // both _configure and initialize are stubs
-    this._configure(options || {});
-    this.initialize.apply(this, arguments);
-  };
 
-  // mix events into the prototype
-  _.extend(Activity.prototype, Backbone.Events, {
 
-    // regions is a map from region names to region objects.
-    // Setup is handled by the base Scaffold.Router's constructor.
-    // This object will be the same for all activities associated with the same router.
-    regions: {},
-
-    // _configure is an empty function by default. Override it with your own
-    // configuration logic.
-    _configure: function() {},
-
-    // Initialize is an empty function by default. Override it with your own
-    // initialization logic.
-    initialize: function(){},
-
-    // The router uses this value to determine whether to call an activity's onCreate callback
-    _initialized: false,
-
-    // updateRegions takes an object of regions by name
-    // For each region given, the corresponding views are inserted. See updateRegion below for details.
-    updateRegions: function(regions) {
-      _.each(regions, function(views, regionName) {
-        this.updateRegion(regionName, views);
-      }, this);
-    },
-
-    // updateRegion takes a region and either a view or an object with a template and a views object.
-    // The views are inserted into the region, replacing any existing views.
-    //
-    // Example: passing a single view:
-    //   updateRegion('main', view);
-    //
-    // Example: passing an array of views (note that if the views' templates are not cached and differ
-    //  then LM does not guarantee that the views will be inserted into the document in order):
-    //   updateRegion('main', [ myView, myOtherView ])
-    //
-    // Example: passing an object of views:
-    //   updateRegion('main', {
-    //     template: 'mytemplate',
-    //     views: {
-    //       '.myclass': myView
-    //     }
-    //   })
-    //
-    updateRegion: function(region, views) {
-
-      // retrieve the actual region by its name
-      region = this.regions[region];
-
-      // beware: hacks; we need to remove the views that were present previously
-      // also set hasRendered to false so that LM doesn't ditch the new views when render is called
-      region._removeViews(true);
-      region.__manager__.hasRendered = false;
-
-      // if we have a single view, insert it directly into the region
-      if (views instanceof Backbone.View) {
-        region.insertView('', views);
-      }
-
-      // if we have an array of views, insert them
-      // beware: if the templates for the views are different then LM may not render them in order!
-      else if (_.isArray(views)) {
-        region.setViews({
-          '': views
-        });
-      }
-      
-      // if we do not have a view, we create a new view to use as the wrapper for the given views,
-      // and insert the given views into it
-      else {
-        region.insertView('', new Backbone.View({ template: views.template}));
-        region.insertViews(views.views);
-      }
-
-      // render the region and all of its views
-      region.render();
-    },
-
-    // callback stubs
-    onCreate: function() {},
-    onStart: function() {},
-    onStop: function() {}
-
-  });
-
-  // use backbone's extend (referencing via View here, but they're all the same)
-  Activity.extend = Backbone.View.extend;
-
-  return {
+  var Scaffold = {
     Model: Backbone.Model.extend({}),
 
     Collection: Backbone.Collection.extend({}),
@@ -153,8 +59,6 @@ define(function(require) {
       }
 
     }),
-
-    Activity: Activity,
 
     Router: Backbone.Router.extend({
 
@@ -247,5 +151,105 @@ define(function(require) {
       }
     })
   };
+
+    // Activity constructor
+  var Activity = function(options) {
+    // both _configure and initialize are stubs
+    this._configure(options || {});
+    this.initialize.apply(this, arguments);
+  };
+
+  // mix events into the prototype
+  _.extend(Activity.prototype, Backbone.Events, {
+
+    // regions is a map from region names to region objects.
+    // Setup is handled by the base Scaffold.Router's constructor.
+    // This object will be the same for all activities associated with the same router.
+    regions: {},
+
+    // _configure is an empty function by default. Override it with your own
+    // configuration logic.
+    _configure: function() {},
+
+    // Initialize is an empty function by default. Override it with your own
+    // initialization logic.
+    initialize: function(){},
+
+    // The router uses this value to determine whether to call an activity's onCreate callback
+    _initialized: false,
+
+    // updateRegions takes an object of regions by name
+    // For each region given, the corresponding views are inserted. See updateRegion below for details.
+    updateRegions: function(regions) {
+      _.each(regions, function(views, regionName) {
+        this.updateRegion(regionName, views);
+      }, this);
+    },
+
+    // updateRegion takes a region and either a view or an object with a template and a views object.
+    // The views are inserted into the region, replacing any existing views.
+    //
+    // Example: passing a single view:
+    //   updateRegion('main', view);
+    //
+    // Example: passing an array of views (note that if the views' templates are not cached and differ
+    //  then LM does not guarantee that the views will be inserted into the document in order):
+    //   updateRegion('main', [ myView, myOtherView ])
+    //
+    // Example: passing an object of views:
+    //   updateRegion('main', {
+    //     template: 'mytemplate',
+    //     views: {
+    //       '.myclass': myView
+    //     }
+    //   })
+    //
+    updateRegion: function(region, views) {
+
+      // retrieve the actual region by its name
+      region = this.regions[region];
+
+      // beware: hacks; we need to remove the views that were present previously
+      // also set hasRendered to false so that LM doesn't ditch the new views when render is called
+      region._removeViews(true);
+      region.__manager__.hasRendered = false;
+
+      // if we have a single view, insert it directly into the region
+      if (views instanceof Backbone.View) {
+        region.insertView('', views);
+      }
+
+      // if we have an array of views, insert them
+      // beware: if the templates for the views are different then LM may not render them in order!
+      else if (_.isArray(views)) {
+        region.setViews({
+          '': views
+        });
+      }
+      
+      // if we do not have a view, we create a new view to use as the wrapper for the given views,
+      // and insert the given views into it
+      else {
+        var wrapperView = new Scaffold.View({ template: views.template});
+        region.insertView('', wrapperView);
+        wrapperView.insertViews(views.views);
+      }
+
+      // render the region and all of its views
+      region.render();
+    },
+
+    // callback stubs
+    onCreate: function() {},
+    onStart: function() {},
+    onStop: function() {}
+
+  });
+
+  // use backbone's extend (referencing via View here, but they're all the same)
+  Activity.extend = Backbone.View.extend;
   
+  Scaffold.Activity = Activity;
+
+  return Scaffold;
 });
